7dba55cce95dfb8ccadb392de2126e9febae9e79
TowaCAI <148300520+TowaCAI@users.noreply.github.com>
2025-08-05 14:52:07 -0300

    feat: add configurable push notifications

--- service-worker.js
+++ service-worker.js
@@ -29,3 +29,15 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
